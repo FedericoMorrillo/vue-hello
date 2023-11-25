5dfe9b52515fade397051a389dd0ea6214ed8375
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-25 11:39:47 +0100

    feat: primo messaggio in Vue

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 'use strict'
+const { createApp } = Vue;
 
+createApp({
+    data() {
+        return {
+            message: 'il mio primo messaggio in vue-js',
+        }
+    }
+}).mount('#app')
